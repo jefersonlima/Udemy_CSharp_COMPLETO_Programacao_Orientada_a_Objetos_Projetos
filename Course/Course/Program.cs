c42828fe83d2a8228189a8be7f993f89b5955bfc
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2022-01-11 08:06:57 -0400

    Aula 16. Tipos básicos de dados em C# - PARTE 2

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,16 +6,70 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            #region Inteiros
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 2147483648;
+            #region Byte
+            sbyte b = sbyte.MinValue;
 
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
+            Console.WriteLine(b);
+            #endregion
+
+            #region Inteiros
+            //byte n1 = 126;
+            //int n2 = 1000;
+            //int n3 = 2147483647;
+            //long n4 = 2147483648;
+
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+            //Console.WriteLine(n3);
+            //Console.WriteLine(n4);
+
+            int nm1 = int.MinValue;
+            int nm2 = int.MaxValue;
+
+            Console.WriteLine(nm1);
+            Console.WriteLine(nm2);
+            #endregion
+
+            #region Float / Double
+            //float n5 = 4.5f; //Float precisa add o f para o c# reconhecer como float
+            //double n6 = 4.5;
+
+            //Console.WriteLine(n5);
+            //Console.WriteLine(n6);
+            #endregion
+
+            #region Decimal
+            decimal d = decimal.MaxValue;
+
+            Console.WriteLine(d);
+            #endregion
+
+            #region Boleanos
+            //bool completo = false;
+
+            //Console.WriteLine(completo);
+            #endregion
+
+            #region Char
+            //char genero = 'f';
+            //char letra = '\u0041'; //pega um caracter pelo seu codigo unicode
+
+            //Console.WriteLine(genero);
+            //Console.WriteLine(letra);
+            #endregion
+
+            #region String
+            //string nome = "Maria Green";
+
+            //Console.WriteLine(nome);
+            #endregion
+
+            #region Object
+            //object obj1 = "Alex Brown";
+            //object obj2 = 4.5f;
+
+            //Console.WriteLine(obj1);
+            //Console.WriteLine(obj2);
             #endregion
         }
     }
