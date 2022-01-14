e0419d76080862cc41267382090f586c9b60df20
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2022-01-14 06:56:23 -0400

    Aula 20. Conversão implícita e casting

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -24,11 +24,11 @@ namespace Course
             //Console.WriteLine(n3);
             //Console.WriteLine(n4);
 
-            int nm1 = int.MinValue;
-            int nm2 = int.MaxValue;
+            //int nm1 = int.MinValue;
+            //int nm2 = int.MaxValue;
 
-            Console.WriteLine(nm1);
-            Console.WriteLine(nm2);
+            //Console.WriteLine(nm1);
+            //Console.WriteLine(nm2);
             #endregion
 
             #region Float / Double
@@ -40,9 +40,9 @@ namespace Course
             #endregion
 
             #region Decimal
-            decimal d = decimal.MaxValue;
+            //decimal d = decimal.MaxValue;
 
-            Console.WriteLine(d);
+            //Console.WriteLine(d);
             #endregion
 
             #region Boleanos
@@ -74,87 +74,113 @@ namespace Course
             #endregion
 
             #region 18. Saída de dados em C#
-            
-            #region Console.Write e Console.WriteLine
-            Console.Write("Bom dia!");
-            Console.WriteLine("Boa tarde!");
-            Console.WriteLine("Boa noite!");
-            #endregion
 
-            Console.WriteLine("----------------------");
+            //#region Console.Write e Console.WriteLine
+            //Console.Write("Bom dia!");
+            //Console.WriteLine("Boa tarde!");
+            //Console.WriteLine("Boa noite!");
+            //#endregion
 
-            #region Imprimindo Variáveis
-            char genero = 'F';
-            int idade = 32;
-            double saldo = 10.35784;
-            string nome = "Maria";
+            //Console.WriteLine("----------------------");
 
-            Console.WriteLine(genero);
-            Console.WriteLine(idade);
-            Console.WriteLine(saldo);
-            Console.WriteLine(nome);
-            #endregion
+            //#region Imprimindo Variáveis
+            //char genero = 'F';
+            //int idade = 32;
+            //double saldo = 10.35784;
+            //string nome = "Maria";
 
-            Console.WriteLine("----------------------");
+            //Console.WriteLine(genero);
+            //Console.WriteLine(idade);
+            //Console.WriteLine(saldo);
+            //Console.WriteLine(nome);
+            //#endregion
 
-            #region Controlando casas decimais
-            Console.WriteLine(saldo.ToString("F2"));
-            Console.WriteLine(saldo.ToString("F4"));
-            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
-            #endregion
+            //Console.WriteLine("----------------------");
 
-            Console.WriteLine("----------------------");
+            //#region Controlando casas decimais
+            //Console.WriteLine(saldo.ToString("F2"));
+            //Console.WriteLine(saldo.ToString("F4"));
+            //Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+            //#endregion
 
-            #region Placeholder
-            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
-            #endregion
+            //Console.WriteLine("----------------------");
 
-            #region Interpolação
-            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
-            #endregion
+            //#region Placeholder
+            //Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+            //#endregion
 
-            #region Concatenação
-            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("f2", CultureInfo.InvariantCulture) + " reais", nome, idade, saldo);
-            #endregion
+            //#region Interpolação
+            //Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+            //#endregion
+
+            //#region Concatenação
+            //Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("f2", CultureInfo.InvariantCulture) + " reais", nome, idade, saldo);
+            //#endregion
 
             #endregion
 
             #region 19. Operadores de atribuição
-            int a = 10;
-            Console.WriteLine(a);
+            //int a = 10, b;
+            //Console.WriteLine(a);
 
-            a += 2;
-            Console.WriteLine(a);
+            //a += 2;
+            //Console.WriteLine(a);
 
-            a *= 3;
-            Console.WriteLine(a);
+            //a *= 3;
+            //Console.WriteLine(a);
 
-            string s = "ABC";
-            Console.WriteLine(s);
+            //string s = "ABC";
+            //Console.WriteLine(s);
 
-            s += "DEF";
-            Console.WriteLine(s);
+            //s += "DEF";
+            //Console.WriteLine(s);
 
-            a = 10;
-            a++;
+            //a = 10;
+            //a++;
 
-            Console.WriteLine(a);
+            //Console.WriteLine(a);
 
-            a = 10;
-            a--;
+            //a = 10;
+            //a--;
 
-            Console.WriteLine(a);
+            //Console.WriteLine(a);
 
-            a = 10;
-            int b = a++;
-            Console.WriteLine(a);
+            //a = 10;
+            //b = a++;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //a = 10;
+            //b = ++a;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            #endregion
+
+            #region 20. Conversão implícita e casting
+            //Conversão de float para double(casting)
+            float a = 4.5f;
+            double b = a;
             Console.WriteLine(b);
 
-            a = 10;
-            b = ++a;
-            Console.WriteLine(a);
-            Console.WriteLine(b);
+            //Conversão de double para float(casting)
+            double c = 5.1;
+            float d = (float)c; 
+            Console.WriteLine(d);
 
+            //Conversão de double para int(casting)
+            double e = 5.1;
+            int f = (int)e;
+            Console.WriteLine(f);
+
+            //conversão de divisão de inteiro para double
+            int g = 5;
+            int h = 2;
+            double resultado = g / h; //vai retornar um inteiro
+            Console.WriteLine(resultado);
+
+            resultado = (double)g / h;
+            Console.WriteLine(resultado);
             #endregion
         }
     }
