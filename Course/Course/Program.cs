c2c515a48fe1b931d9779bd0612623f34cf262c4
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2022-01-19 07:28:30 -0400

    Aula 23. Entrada de dados em C# - PARTE 2

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -215,29 +215,54 @@ namespace Course
             #endregion
 
             #region 22. Entrada de dados em C# - PARTE 1
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
+            //string frase = Console.ReadLine();
+            //string x = Console.ReadLine();
+            //string y = Console.ReadLine();
+            //string z = Console.ReadLine();
 
-            Console.WriteLine("Você digitou: ");
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            //Console.WriteLine("Você digitou: ");
+            //Console.WriteLine(frase);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
 
-            #region Split
-            //string s = Console.ReadLine();
-            //string[] v = s.Split(' ');
-            string[] v = Console.ReadLine().Split(' ');
-            string a = v[0];
-            string b = v[1];
-            string c = v[2];
+            //#region Split
+            ////string s = Console.ReadLine();
+            ////string[] v = s.Split(' ');
+            //string[] v = Console.ReadLine().Split(' ');
+            //string a = v[0];
+            //string b = v[1];
+            //string c = v[2];
 
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //#endregion
             #endregion
+
+            #region 23. Entrada de dados em C# - PARTE 2
+            int n1 = int.Parse(Console.ReadLine());
+            //int n1_1 = Convert.ToInt32(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("f2", CultureInfo.InvariantCulture));
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+            Console.WriteLine(altura.ToString("f2", CultureInfo.InvariantCulture));
+
             #endregion
         }
     }
