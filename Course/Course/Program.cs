d6a5e1f0b431ef58253a2da84931257feba261ba
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2022-02-03 06:28:01 -0400

    Aula 68. Nullable.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -883,13 +883,53 @@ namespace Course
             #endregion
 
             #region 66. Tipos referência e tipos valor
-            Point p;
-            p.X = 10;
-            p.Y = 20;
+            //Point p;
+            //p.X = 10;
+            //p.Y = 20;
 
-            Console.WriteLine(p);
-            p = new Point();
-            Console.WriteLine(p);
+            //Console.WriteLine(p);
+            //p = new Point();
+            //Console.WriteLine(p);
+            #endregion
+
+            #region 68. Nullable
+            Nullable<double> x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+            }
+
+            #region Operador de coalescência "??"
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            #endregion
+
+
+
             #endregion
 
 
