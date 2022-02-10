a5c6fd6d1cdd6cc34a86a9d14409417f278e8e58
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2022-02-10 13:49:47 -0400

    Aula 90. TimeSpan - representando durações

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1479,41 +1479,79 @@ namespace Course
             #endregion
 
             #region 89. DateTime - representando data e hora
-            DateTime d1 = DateTime.Now;
+            //DateTime d1 = DateTime.Now;
 
-            Console.WriteLine(d1);
-            Console.WriteLine(d1.Ticks);
+            //Console.WriteLine(d1);
+            //Console.WriteLine(d1.Ticks);
 
-            DateTime d2 = new DateTime(2018, 11, 25);
-            Console.WriteLine(d2);
-            d2 = new DateTime(2018, 11, 25, 20, 45, 3);
-            Console.WriteLine(d2);
-            d2 = new DateTime(2018, 11, 25, 20, 45, 3, 500);
-            Console.WriteLine(d2);
+            //DateTime d2 = new DateTime(2018, 11, 25);
+            //Console.WriteLine(d2);
+            //d2 = new DateTime(2018, 11, 25, 20, 45, 3);
+            //Console.WriteLine(d2);
+            //d2 = new DateTime(2018, 11, 25, 20, 45, 3, 500);
+            //Console.WriteLine(d2);
 
-            d2 = DateTime.Now;
-            Console.WriteLine(d2);
+            //d2 = DateTime.Now;
+            //Console.WriteLine(d2);
 
-            d2 = DateTime.Today;
-            Console.WriteLine(d2);
+            //d2 = DateTime.Today;
+            //Console.WriteLine(d2);
 
-            d2 = DateTime.UtcNow;
-            Console.WriteLine(d2);
+            //d2 = DateTime.UtcNow;
+            //Console.WriteLine(d2);
 
-            d2 = DateTime.Parse("2000-08-15");
-            Console.WriteLine(d2);
-            d2 = DateTime.Parse("2000-08-15 13:05:58");
-            Console.WriteLine(d2);
-            d2 = DateTime.Parse("15/08/2000");
-            Console.WriteLine(d2);
-            d2 = DateTime.Parse("15/08/2000 13:05:58");
-            Console.WriteLine(d2);
+            //d2 = DateTime.Parse("2000-08-15");
+            //Console.WriteLine(d2);
+            //d2 = DateTime.Parse("2000-08-15 13:05:58");
+            //Console.WriteLine(d2);
+            //d2 = DateTime.Parse("15/08/2000");
+            //Console.WriteLine(d2);
+            //d2 = DateTime.Parse("15/08/2000 13:05:58");
+            //Console.WriteLine(d2);
 
-            d2 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
-            Console.WriteLine(d2);
+            //d2 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            //Console.WriteLine(d2);
 
-            d2 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
-            Console.WriteLine(d2);
+            //d2 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            //Console.WriteLine(d2);
+
+            #endregion
+
+            #region 90. TimeSpan - representando durações
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            Console.WriteLine(t1);
+
+            Console.WriteLine(t1.Ticks);
+
+            t1 = new TimeSpan();
+            Console.WriteLine(t1);
+
+            t1 = new TimeSpan(900000000L);
+            Console.WriteLine(t1);
+
+            t1 = new TimeSpan(2, 11, 21);
+            Console.WriteLine(t1);
+
+            t1 = new TimeSpan(1, 2, 11, 21);
+            Console.WriteLine(t1);
+
+            t1 = new TimeSpan(1, 2, 11, 21, 321);
+            Console.WriteLine(t1);
+
+            t1 = TimeSpan.FromDays(1.5);
+            Console.WriteLine(t1);
+
+            t1 = TimeSpan.FromHours(1.5);
+            Console.WriteLine(t1);
+
+            t1 = TimeSpan.FromMinutes(1.5);
+            Console.WriteLine(t1);
+
+            t1 = TimeSpan.FromSeconds(1.5);
+            Console.WriteLine(t1);
+
+            t1 = TimeSpan.FromMilliseconds(1.5);
+            Console.WriteLine(t1);
 
             #endregion
         }
