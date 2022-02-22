4cc951038817f39fd95e5def6a133d1fed6e0d54
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2022-02-22 07:47:42 -0400

    Inicio Projeto Xadrez

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -2309,39 +2309,45 @@ namespace Course
             #endregion
 
             #region 146. Exercício proposto (exceções)
-            try
-            {
-                Console.WriteLine("Enter account data");
-                Console.Write("Number: ");
-                int number = int.Parse(Console.ReadLine());
+            //try
+            //{
+            //    Console.WriteLine("Enter account data");
+            //    Console.Write("Number: ");
+            //    int number = int.Parse(Console.ReadLine());
 
-                Console.Write("Holder: ");
-                string holder = Console.ReadLine();
+            //    Console.Write("Holder: ");
+            //    string holder = Console.ReadLine();
 
-                Console.Write("Initial balance: ");
-                double initialBalance = double.Parse(Console.ReadLine());
+            //    Console.Write("Initial balance: ");
+            //    double initialBalance = double.Parse(Console.ReadLine());
 
-                Console.Write("Withdraw limit: ");
-                double withdrawLimit = double.Parse(Console.ReadLine());
+            //    Console.Write("Withdraw limit: ");
+            //    double withdrawLimit = double.Parse(Console.ReadLine());
 
-                AccountClass146 accountClass146 = new AccountClass146(number, holder, initialBalance, withdrawLimit);
+            //    AccountClass146 accountClass146 = new AccountClass146(number, holder, initialBalance, withdrawLimit);
 
-                Console.WriteLine();
-                Console.Write("Enter amount for withdraw: ");
-                accountClass146.Withdraw(double.Parse(Console.ReadLine()));
+            //    Console.WriteLine();
+            //    Console.Write("Enter amount for withdraw: ");
+            //    accountClass146.Withdraw(double.Parse(Console.ReadLine()));
 
-                Console.WriteLine("New balance: {0}",accountClass146.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            //    Console.WriteLine("New balance: {0}",accountClass146.Balance.ToString("F2", CultureInfo.InvariantCulture));
 
-            }
-            catch (DomainException e)
-            {
-                Console.WriteLine("Withdraw error: {0}", e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected error: {0}", e.Message);
-            }
+            //}
+            //catch (DomainException e)
+            //{
+            //    Console.WriteLine("Withdraw error: {0}", e.Message);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Unexpected error: {0}", e.Message);
+            //}
             #endregion
+
+            #region Projeto de Xadrez
+            //Esse projeto foi feito em outra solutions chamada xadrez_console
+            #endregion
+
+
 
         }
         #region Função para calcular a média de um array do tipo double
